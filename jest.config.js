5e0a888d389241b4c14dae5bda323947dd213150
mckrava <mckrava2@gmail.com>
2021-11-28 12:42:21 +0100

    Add test

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,5 @@ module.exports = {
   transform: {
     '^.+\\.(ts)$': 'ts-jest',
   },
+  setupFiles: ["<rootDir>/jest.env.js"],
 }
--- tests/polkadot-dapp.test.ts
+++ tests/polkadot-dapp.test.ts
@@ -1,64 +1,42 @@
-import { Page, ChromiumBrowserContext } from 'playwright'
+import { Page, ChromiumBrowserContext } from 'playwright';
+// import { ElementHandleForTag } from 'playwright-core';
 import {
   MatchImageSnapshotOptions,
   toMatchImageSnapshot,
-} from 'jest-image-snapshot'
+} from 'jest-image-snapshot';
 import {
   CLOSE_PAGES,
   initBrowserWithExtension,
-} from './utils'
+  initPolkadotDapp,
+} from './utils';
 
-expect.extend({ toMatchImageSnapshot })
+expect.extend({ toMatchImageSnapshot });
 
+let page: Page;
+let browserContext: ChromiumBrowserContext;
+let extensionURL: string;
 
-const aliceAccount = 'bXmPf7DcVmFuHEmzH3UX8t6AUkfNQW8pnTeXGhFhqbfngjAak';
-const testAccount = 'bXn2PvP753f2YAKacPbAJo9WazQuHT5dsaNPciyQFUaEfqbpe';
-const testAccountName = 'E2ETESTACC1';
-const testAccountPass = 'qwerty';
-const testAccountSeed = 'category city hire ramp explain duty garment mask draw submit bar appear';
+describe('The Extension page should', () => {
+  beforeAll(async () => {
+    const init = await initBrowserWithExtension();
 
-let page: Page
-let browserContext: ChromiumBrowserContext
-let extensionURL: string
+    browserContext = init.browserContext;
+    extensionURL = init.extensionURL;
 
-const initPolkadotDapp = async () => {
-  const init = await initBrowserWithExtension()
-  browserContext = init.browserContext
-  extensionURL = init.extensionURL
-  page = browserContext.pages()[0]
-  // const newSession = await browserContext.newCDPSession(page);
-  // newSession.tracing.start({ screenshots: true, snapshots: true });
+    await browserContext.tracing.start({ screenshots: true, snapshots: true });
 
-  await page.goto(extensionURL);
-  await page.bringToFront()
+    page = await initPolkadotDapp(browserContext, extensionURL);
+    // page = browserContext.pages()[0]
+  });
 
-  await page.click(".Button-sc-1gyneog-0");
-  await page.click(".popupMenus .popupToggle");
-  await page.click('.menuItem a[href="#/account/import-seed"]');
-  await page.fill('textarea[class*="TextInputs__TextArea-sc"]', testAccountSeed);
-  await page.click('button[class*=Button-]');
-  await page.fill('input[type=text]', testAccountName);
-  await page.fill('input[type=password]', testAccountPass);
-  await page.fill('//label[(text()="Repeat password for verification")]/following-sibling::input', testAccountPass);
-  await page.click('//div[(text()="Add the account with the supplied seed")]/..');
+  afterAll(async () => {
+    await browserContext.tracing.stop({ path: './trace.zip' });
+    await browserContext?.close();
+    browserContext = null;
+    page = null;
+    extensionURL = '';
+  });
 
-}
-
-describe('The Extension page should', async () => {
-  // beforeAll(async () => {
-  //   const init = await initBrowserWithExtension()
-  //   browserContext = init.browserContext
-  //   extensionURL = init.extensionURL
-  //   page = browserContext.pages()[0]
-  // })
-  //
-  // afterAll(async () => {
-  //   await browserContext?.close()
-  //   browserContext = null
-  //   page = null
-  //   extensionURL = ''
-  // })
-  //
   // beforeEach(async () => {
   //   if (!extensionURL) {
   //     console.error('Invalid extensionURL', { extensionURL })
@@ -69,29 +47,76 @@ describe('The Extension page should', async () => {
   //   await CLOSE_PAGES(browserContext)
   // })
 
-  await initPolkadotDapp();
-  await page.waitForTimeout(2000);
-  // await page.goto('https://polkadot.js.org/apps/#/explorer');
-  await page.goto('https://polkadot.js.org/apps/?rpc=ws%3A%2F%2F127.0.0.1%3A9988#/accounts');
-  console.log('>>> page ', await page.title())
+  // await initPolkadotDapp();
+  // await page.waitForTimeout(2000);
+  // await browserContext.tracing.stop({ path: './trace.zip' });
+  //
+  // // await page.goto('https://polkadot.js.org/apps/#/explorer');
+  // await page.goto(
+  //   'https://polkadot.js.org/apps/?rpc=ws%3A%2F%2F127.0.0.1%3A9988#/accounts'
+  // );
+  // console.log('>>> page ', await page.title());
+  //
+  // browserContext.on('page', async confPage => {
+  //   await confPage.waitForLoadState();
+  //   await confPage.click(
+  //     '//div[(text()="Yes, allow this application access")]/..'
+  //   );
+  // });
+  //
+  // await page.waitForTimeout(2000);
+  // await page.click(
+  //   `//div[text()="${process.env.TEST_ACCOUNT_NAME} (EXTENSION)"]/ancestor-or-self::tr/descendant-or-self::button[contains(@class, "send-button")]`
+  // );
+  // await page.click(
+  //   `//div[contains(@class, "app--accounts-Modal")]/descendant-or-self::label[text()="send to address"]/../descendant-or-self::i`
+  // );
+  // await page.click(
+  //   `//div[contains(@class, "app--accounts-Modal")]/descendant-or-self::label[text()="send to address"]/../descendant-or-self::div[@name="alice"]/descendant-or-self::div[@class="address"]`,
+  //   { force: true }
+  // );
+  // await page.fill(
+  //   `//div[contains(@class, "app--accounts-Modal")]/descendant-or-self::label[text()="amount"]/../descendant-or-self::input`,
+  //   '3'
+  // );
+  // await page.click(
+  //   `//div[contains(@class, "app--accounts-Modal")]/descendant-or-self::button[text()="Make Transfer"]`
+  // );
+  // await page.click(
+  //   `//div[@data-testid="modal"]/descendant-or-self::button[text()="Sign and Submit"]`
+  // );
+  //
+  // browserContext.on('page', async confPage => {
+  //   await confPage.waitForLoadState('load');
+  //   await confPage.fill(
+  //     '//body/descendant-or-self::input[@type="password"]',
+  //     process.env.TEST_ACCOUNT_PASS
+  //   );
+  //   await confPage.click(
+  //     '//body/descendant-or-self::div[(text()="Sign the transaction")]/..'
+  //   );
+  // });
 
-  browserContext.on('page', async confPage => {
-    await confPage.waitForLoadState();
-    await confPage.click('//div[(text()="Yes, allow this application access")]/..');
-  })
+  it('Open Polkadot UI', async () => {
+    // await page.goto('https://polkadot.js.org/apps/?rpc=ws%3A%2F%2F127.0.0.1%3A9988#/accounts');
+    await page.goto(
+      'https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Fbasilisk.api.onfinality.io%2Fpublic-ws#/accounts'
+    );
 
-  await page.waitForTimeout(2000);
-  await page.click(`//div[text()="${testAccountName} (EXTENSION)"]/ancestor-or-self::tr/descendant-or-self::button[contains(@class, "send-button")]`)
-  await page.click(`//div[contains(@class, "app--accounts-Modal")]/descendant-or-self::label[text()="send to address"]/../descendant-or-self::i`)
-  await page.click(`//div[contains(@class, "app--accounts-Modal")]/descendant-or-self::label[text()="send to address"]/../descendant-or-self::div[@name="alice"]/descendant-or-self::div[@class="address"]`, { force: true })
-  await page.fill(`//div[contains(@class, "app--accounts-Modal")]/descendant-or-self::label[text()="amount"]/../descendant-or-self::input`, '3')
-  await page.click(`//div[contains(@class, "app--accounts-Modal")]/descendant-or-self::button[text()="Make Transfer"]`)
-  await page.click(`//div[@data-testid="modal"]/descendant-or-self::button[text()="Sign and Submit"]`)
+    browserContext.on('page', async confPage => {
+      await confPage.waitForLoadState();
+      await confPage.click(
+        '//div[(text()="Yes, allow this application access")]/..'
+      );
+    });
 
-  browserContext.on('page', async confPage => {
-    await confPage.waitForLoadState('load');
-    await confPage.fill('//body/descendant-or-self::input[@type="password"]', testAccountPass);
-    await confPage.click('//body/descendant-or-self::div[(text()="Sign the transaction")]/..');
-  })
-  // await browserContext.tracing.stop({ path: 'trace.zip' });
-})
+    await page.waitForTimeout(2000);
+    const testAccItem = await page.waitForSelector(
+      `//div[text()="${process.env.TEST_ACCOUNT_NAME} (EXTENSION)"]/ancestor-or-self::tr`,
+      { timeout: 20000 }
+    );
+
+    // await expect(testAccItem).resolves.any(ElementHandleForTag);
+    await expect(testAccItem).not.toBe(null);
+  });
+});
